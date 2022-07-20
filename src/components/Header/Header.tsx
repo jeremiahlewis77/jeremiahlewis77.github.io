4543b9b9583d55382f82fa325c72b218f603082f
Jeremiah Lewis <28968799+jeremiahlewis77@users.noreply.github.com>
2022-07-20 02:36:22 -0500

    Update Header.tsx component
    This commit updates the Header.tsx component by:
    - Changing to a default light theme
    - Adding responsive rules and hover animations to elements
    - Revising menu options
    
    The hamburger menu has not yet been implemented for mobile devices.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,21 @@
 import Link from "next/link";
 import Image from "next/image";
 import logoWhite from '../../public/logo-white.svg'
-
+import logo from '../../public/logo.svg'
 
 function Header() {
     return (
-        <header className="p-5 bg-gunmetal antialiased">
+        <header className="p-5 bg-snow">
             <div className="flex items-center justify-between max-w-7xl mx-auto md:px-5">
-                <div className="w-40 cursor-pointer">
-                    <Link href="/"><Image src={logoWhite} alt="Jeremiah L." layout="responsive"/></Link>
+                <div className="sm:w-40 w-32 cursor-pointer hover:translate-y-1 transition ease-in-out">
+                    <Link href="/"><Image src={logo} alt="Jeremiah L." layout="responsive"/></Link>
                 </div>
-                <div className="inline-flex space-x-6 items-center text-white">
-                    <h3>About</h3>
-                    <h3>Projects</h3>
-                    <h3 className="bg-white text-gunmetal p-2 px-4 rounded-full">Contact</h3>
+                <div className="hidden md:inline-flex sm:space-x-6 space-x-4 items-center text-gunmetal font-medium">
+                    <h3 className="link-underline link-underline-black">About</h3>
+                    <h3 className="link-underline link-underline-black">Projects</h3>
+                    <h3 className="link-underline link-underline-black">Resume</h3>
+                    <h3 className="link-underline link-underline-black">Services</h3>
+                    <h3 className="bg-gunmetal text-white p-1 px-3 rounded-full hover:bg-sapphire transition ease-in-out">Contact</h3>
                 </div>
             </div>
         </header>
