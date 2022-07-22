3373a6ed9108231a63287c7216fc1e702a49b9c5
Jeremiah Lewis <28968799+jeremiahlewis77@users.noreply.github.com>
2022-07-22 17:55:06 -0500

    Configure webpack to load PDF files

--- src/next.config.js
+++ src/next.config.js
@@ -1,25 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-  webpack: (config) => {
-    // load worker files as a urls with `file-loader`
-    config.module.rules.unshift({
-      test: /pdf\.worker\.(min\.)?js/,
-      use: [
-        {
-          loader: "file-loader",
-          options: {
-            name: "[contenthash].[ext]",
-            publicPath: "_next/static/worker",
-            outputPath: "static/worker"
-          }
-        }
-      ]
-    });
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.pdf$/i,
+      type: 'asset/source'
+    })
 
-    return config;
-  }
-}
-
-
-module.exports = nextConfig
+    return config
+  },
+};
