e8de8d23f3b9dc032220d8aee49c889e39437f6b
Jeremiah Lewis <28968799+jeremiahlewis77@users.noreply.github.com>
2022-07-22 17:49:48 -0500

    Update pdf-viewer.js

--- src/components/pdf-viewer.js
+++ src/components/pdf-viewer.js
@@ -1,13 +1,12 @@
 import { useState } from "react";
-// import default react-pdf entry
 import { Document, Page, pdfjs } from "react-pdf";
-import ResumeLink from "../public/Resume.pdf"
+import ResumeLink from "../public/Jeremiah Lewis - Resume.pdf"
 // import pdf worker as a url, see `next.config.js` and `pdf-worker.js`
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 
 export default function PDFViewer({ size }) {
-    const [file, setFile] = useState("../public/Resume.pdf");
+    const [file, setFile] = useState("../public/Jeremiah Lewis - Resume.pdf");
     const [numPages, setNumPages] = useState(null);
 
     function onFileChange(event) {
