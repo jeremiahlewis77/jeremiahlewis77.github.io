d437c098fe029d78a9b4d19c315459d22ef2fb01
Jeremiah Lewis <28968799+jeremiahlewis77@users.noreply.github.com>
2022-07-23 00:51:04 -0500

    :bug: Fix issue where resume PDF fails to load
    This commit fixes issue by fetching PDF from GitHub instead of using local PDF file.

--- src/components/pdf-viewer.js
+++ src/components/pdf-viewer.js
@@ -1,17 +1,12 @@
 import { useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
-import ResumeLink from "../public/Jeremiah Lewis - Resume.pdf"
 // import pdf worker as a url, see `next.config.js` and `pdf-worker.js`
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
+export const ResumeLink = "https://raw.githubusercontent.com/jeremiahlewis77/personal-website/develop/src/public/Jeremiah%20Lewis%20-%20Resume.pdf"
 
 export default function PDFViewer({ size }) {
-    const [file, setFile] = useState("../public/Jeremiah Lewis - Resume.pdf");
     const [numPages, setNumPages] = useState(null);
-
-    function onFileChange(event) {
-        setFile(event.target.files[0]);
-    }
 
     function onDocumentLoadSuccess({ numPages: nextNumPages }) {
         setNumPages(nextNumPages);
