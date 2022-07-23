aa9813f944fe92d4badd76b8aeb20d8ecbd8cd72
Jeremiah Lewis <28968799+jeremiahlewis77@users.noreply.github.com>
2022-07-22 23:59:57 -0500

    Import next/router and links

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,8 +2,11 @@ import Link from "next/link";
 import Image from "next/image";
 import logoWhite from '../../public/logo-white.svg'
 import logo from '../../public/logo.svg'
+import { useRouter } from 'next/router';
 
 function Header() {
+    const router = useRouter();
+
     return (
         <header className="p-5 bg-snow">
             <div className="flex items-center justify-between max-w-7xl mx-auto md:px-5">
@@ -11,10 +14,14 @@ function Header() {
                     <Link href="/"><Image src={logo} alt="Jeremiah L." layout="responsive"/></Link>
                 </div>
                 <div className="hidden md:inline-flex sm:space-x-6 space-x-4 items-center text-gunmetal font-medium">
-                    <h3 className="link-underline link-underline-black">About</h3>
-                    <h3 className="link-underline link-underline-black">Projects</h3>
-                    <h3 className="link-underline link-underline-black">Resume</h3>
-                    <h3 className="link-underline link-underline-black">Services</h3>
+                    <h3 className={"link-underline link-underline-black " + (router.pathname == "/about" ? "current" : "")}>About</h3>
+                    <h3 className={"link-underline link-underline-black " + (router.pathname == "/projects" ? "current" : "")}>Projects</h3>
+                    <h3 className={"link-underline link-underline-black " + (router.pathname == "/resume" ? "current" : "")}>
+                        <Link href="/resume" passHref={true}>
+                            <a>Resume</a>
+                        </Link>
+                    </h3>
+                    <h3 className={"link-underline link-underline-black " + (router.pathname == "/services" ? "current" : "")}>Services</h3>
                     <h3 className="bg-gunmetal text-white p-1 px-3 rounded-full hover:bg-sapphire transition ease-in-out">Contact</h3>
                 </div>
             </div>
