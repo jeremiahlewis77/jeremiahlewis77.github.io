30728c9e130e061c614aa8cecbd60649dc046b4d
Jeremiah Lewis <28968799+jeremiahlewis77@users.noreply.github.com>
2022-08-21 17:08:29 -0500

    :sparkles: Implement mobile navigation
    This commit implements the mobile navigation by using useState to toggle visibility and CSS needed to make separate styles for desktop and mobile.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -25,31 +25,31 @@ function Header() {
                               clipRule="evenodd"/>
                     </svg>
                 </button>
-                <div className={`${showMobileMenu ? "" : "hidden"} w-full md:block md:w-auto w-fit bg-gray-100 md:bg-transparent mt-4 md:mt-0 shadow`} id="mobile-menu">
+                <div className={`${showMobileMenu ? "" : "hidden"} w-full md:block md:w-auto w-fit bg-gray-100 md:bg-transparent mt-4 md:mt-0 shadow md:shadow-none`} id="mobile-menu">
                     <ul className="flex flex-col p-4 md:flex-row md:space-x-8 md:mt-0 md:text-md font-medium md:border-0">
                         <li>
                             <Link href={"/"} passHref>
-                            <a href="#" className={`block py-2 pr-4 pl-3 text-gunmetal rounded md:rounded-none md:bg-transparent md:p-0 md:link-underline md:hover:bg-transparent ${(router.pathname == "/" ? "current md:text-sapphire bg-sapphire text-white" : "")}`}>Home</a>
+                            <a href="#" className={`block py-2 pr-4 pl-3 text-gunmetal rounded md:rounded-none md:bg-transparent md:p-0 link-underline hover:bg-gray-300 md:hover:bg-transparent ${(router.pathname == "/" ? "current md:text-sapphire bg-sapphire text-white" : "")}`}>Home</a>
                             </Link>
                         </li>
                         <li>
                             <Link href={"/about"} passHref>
-                                <a href="#" className={`block py-2 pr-4 pl-3 text-gunmetal rounded md:rounded-none md:bg-transparent md:p-0 md:link-underline md:hover:bg-transparent ${(router.pathname == "/about" ? "current md:text-sapphire bg-sapphire text-white" : "")}`}>About</a>
+                                <a href="#" className={`block py-2 pr-4 pl-3 text-gunmetal rounded md:rounded-none md:bg-transparent md:p-0 link-underline hover:bg-gray-300 md:hover:bg-transparent ${(router.pathname == "/about" ? "current md:text-sapphire bg-sapphire text-white" : "")}`}>About</a>
                             </Link>
                         </li>
                         <li>
                             <Link href={"/projects"} passHref>
-                                <a className={`block py-2 pr-4 pl-3 text-gunmetal rounded md:rounded-none md:bg-transparent md:p-0 md:link-underline md:hover:bg-transparent ${(router.pathname == "/projects" ? "current md:text-sapphire bg-sapphire text-white" : "")}`}>Projects</a>
+                                <a className={`block py-2 pr-4 pl-3 text-gunmetal rounded md:rounded-none md:bg-transparent md:p-0 link-underline hover:bg-gray-300 md:hover:bg-transparent ${(router.pathname == "/projects" ? "current md:text-sapphire bg-sapphire text-white" : "")}`}>Projects</a>
                             </Link>
                         </li>
                         <li>
                             <Link href={"/resume"} passHref>
-                                <a className={`block py-2 pr-4 pl-3 md-text-gunmetal rounded md:rounded-none md:bg-transparent md:p-0 md:link-underline md:hover:bg-transparent ${(router.pathname == "/resume" ? "current md:text-sapphire bg-sapphire text-white" : "")}`}>Resume</a>
+                                <a className={`block py-2 pr-4 pl-3 md-text-gunmetal rounded md:rounded-none md:bg-transparent md:p-0 link-underline hover:bg-gray-300 md:hover:bg-transparent ${(router.pathname == "/resume" ? "current md:text-sapphire bg-sapphire text-white" : "")}`}>Resume</a>
                             </Link>
                         </li>
                         <li>
                             <Link href={"/contact"} passHref>
-                                <a className={`block py-2 pr-4 pl-3 text-gunmetal rounded md:rounded-none md:bg-transparent md:p-0 md:link-underline hover:bg-gray-300 md:hover:bg-transparent ${(router.pathname == "/contact" ? "current md:text-sapphire bg-sapphire text-white" : "")}`}>Contact</a>
+                                <a className={`block py-2 pr-4 pl-3 text-gunmetal rounded md:rounded-none md:bg-transparent md:p-0 link-underline hover:bg-gray-300 md:hover:bg-transparent ${(router.pathname == "/contact" ? "current md:text-sapphire bg-sapphire text-white" : "")}`}>Contact</a>
                             </Link>
                         </li>
                     </ul>
