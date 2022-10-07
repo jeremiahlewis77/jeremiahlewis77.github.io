50a712b6bc976d7432eb4371b3a0417c64f22484
Jeremiah Lewis <28968799+jeremiahlewis77@users.noreply.github.com>
2022-10-07 02:15:31 -0500

    Change conditional rendering for links from AND to OR logic

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -28,18 +28,21 @@ function Project({ id, slug, title, description, githubURL, liveDemo, coverImage
             <div className="p-4">
                 <div>
                     <h3 className="text-sapphire font-bold text-2xl tracking-tight leading-tight">{title}</h3>
-                    {(githubURL != null) && (liveDemo != null) ?
+                    {(githubURL != null) || (liveDemo != null) ?
                         <div className="inline flex space-x-2 mb-2">
-                            <a className="flex items-center link" href={githubURL}>
+                            {(githubURL != null) ?
+                                <a className="flex items-center link" href={githubURL}>
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1"
-                                     viewBox="0 0 20 20"
-                                     fill="currentColor">
-                                    <path fillRule="evenodd"
-                                          d="M12.316 3.051a1 1 0 01.633 1.265l-4 12a1 1 0 11-1.898-.632l4-12a1 1 0 011.265-.633zM5.707 6.293a1 1 0 010 1.414L3.414 10l2.293 2.293a1 1 0 11-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0zm8.586 0a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 11-1.414-1.414L16.586 10l-2.293-2.293a1 1 0 010-1.414z"
-                                          clipRule="evenodd"/>
+                                viewBox="0 0 20 20"
+                                fill="currentColor">
+                                <path fillRule="evenodd"
+                                d="M12.316 3.051a1 1 0 01.633 1.265l-4 12a1 1 0 11-1.898-.632l4-12a1 1 0 011.265-.633zM5.707 6.293a1 1 0 010 1.414L3.414 10l2.293 2.293a1 1 0 11-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0zm8.586 0a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 11-1.414-1.414L16.586 10l-2.293-2.293a1 1 0 010-1.414z"
+                                clipRule="evenodd"/>
                                 </svg>
                                 Source</a>
-                            <span className="text-gray-300">|</span>
+                            : ""}
+                            {(githubURL != null) && (liveDemo != null) ? <span className="text-gray-300">|</span> : ""}
+                            {(liveDemo != null) ?
                             <a className="flex items-center link" href={liveDemo}>
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 rotate-45 mr-1"
                                      viewBox="0 0 20 20"
@@ -49,12 +52,13 @@ function Project({ id, slug, title, description, githubURL, liveDemo, coverImage
                                           clipRule="evenodd"/>
                                 </svg>
                                 Live</a>
+                            : ""}
                         </div>
                         : ""}
                 </div>
                 <p className="tracking-tight mb-2">{description}</p>
                 <div className={`text-3xl flex items-center space-x-4 ${(markdown == null) ? "mb-2" : "mb-4"}`}>
-                    {technologies.map((tech: string, index: any) => (
+                    {technologies.map((tech: string, index: number) => (
                         <TechIcon key={index} technology={tech}/>
                     ))}
                 </div>
