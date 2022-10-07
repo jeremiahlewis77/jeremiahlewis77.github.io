53ff73cdf7a4d69cb9d3b5e279ee668df62e2e45
Jeremiah Lewis <28968799+jeremiahlewis77@users.noreply.github.com>
2022-10-07 02:36:35 -0500

    Extract constants
    This commit extracts constants and allows them to be imported into the pages that need them.

---
+++ src/constants/technologies.ts
@@ -0,0 +1,23 @@
+const technologies = [
+    {tech: "cplusplus", title: "C++", link: "https://cplusplus.com"},
+    {tech: "javascript", title: "JavaScript", link: "https://javascript.com"},
+    {tech: "typescript", title: "TypeScript", link: "https://typescriptlang.org"},
+    {tech: "python", title: "Python", link: "https://python.org"},
+    {tech: "git", title: "Git", link: "https://reactjs.org"},
+    {tech: "mysql", title: "MySQL", link: "https://www.mysql.com/"},
+    {tech: "postgresql", title: "PostgreSQL", link: "https://www.postgresql.org/"},
+    {tech: "docker", title: "Docker", link: "https://docker.com"},
+    {tech: "html5", title: "HTML", link: "https://en.wikipedia.org/wiki/HTML"},
+    {tech: "css3", title: "CSS", link: "https://en.wikipedia.org/wiki/CSS"},
+    {tech: "amazonwebservices", title: "AWS", link: "https://aws.amazon.com/"}
+]
+
+const frameworks = [
+    {tech: "react", title: "React.js", link: "https://reactjs.org"},
+    {tech: "nextjs", title: "Next.js", link: "https://nextjs.org/"},
+    {tech: "django", title: "Django", link: "https://www.djangoproject.com/"},
+    {tech: "tailwindcss", title: "TailwindCSS", link: "https://tailwindcss.com/"},
+    {tech: "bootstrap", title: "Bootstrap", link: "https://getbootstrap.com/"},
+]
+
+export {technologies, frameworks}
