eac41f67a3a5cbcada85bf3cf8c9dea48bd94bdf
Jeremiah Lewis <28968799+jeremiahlewis77@users.noreply.github.com>
2023-12-11 00:59:55 -0600

    Fix unescaped entities in construction.tsx

--- src/pages/construction.tsx
+++ src/pages/construction.tsx
@@ -10,7 +10,7 @@ const ConstructionPage = () => {
             <div className="flex justify-center items-center h-screen bg-gray-200">
                 <div className="text-center">
                     <h1 className="text-4xl font-semibold text-sapphire">Our Website is Under Construction</h1>
-                    <p className="mt-2">We&aposre working on something exciting! We&aposll be back soon.</p>
+                    <p className="mt-2">We&apos;re working on something exciting! We&apos;ll be back soon.</p>
                 </div>
             </div>
         </>
